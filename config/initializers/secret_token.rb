7dac167d6cb564105dcc7f0aaa1efc65b014a334
Paulius <p.kiselis@svako.lt>
2012-01-24 15:16:46 +0200

    pirmas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandymas::Application.config.secret_token = '447cd2fcf197678853faa88aae5d2153a26468710c87302d5ad17bda6a6e355e09f195cd710a457daa0a2a1488a84737ba5b4b80f98a91853202d40ce591c72b'
